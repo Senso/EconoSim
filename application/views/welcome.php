dadb27d4dd801300e0e48f69027c2a1ab575c343
Senso <sensoz@gmail.com>
2012-05-08 20:40:56 +0700

    deeerp

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -6,7 +6,7 @@ Hi, <strong><?php echo $username; ?></strong>! You are logged in now. <?php echo
 
 <?php
     if ($company) {
-        echo "Your company is <a href='/company/info/" . $company[0]->id . "'>" . ${company[0]}->name . "</a>.";
+        echo "Your company is <a href='/company/info/" . $company[0]->id . "'>" . ${company[0]->name . "</a>.";
     }
     else {
         echo 'You do not have a company created yet. <a href="/company/create/">Create one</a>.';
