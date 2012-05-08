5436f113243337cb87e78922c873e6f40196d3fd
Sensoz <sensoz@gmail.com>
2012-05-08 16:03:48 +0700

    fixed url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://donginger.info/econosim/';
+$config['base_url']	= 'http://donginger.info/';
 
 /*
 |--------------------------------------------------------------------------
