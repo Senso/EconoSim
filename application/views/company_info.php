4a258904d45b60e13e9b0a92700e3265a500bbcc
Senso <sensoz@gmail.com>
2012-05-08 22:46:29 +0700

    added cash display

--- application/views/company_info.php
+++ application/views/company_info.php
@@ -7,6 +7,7 @@
 Name: <?php echo $info->name; ?> <br />
 Owner: <a href='/user/info/<?php echo $info->owner;?>'><?php echo $owner_info->username; ?></a> <br />
 Created: <?php echo $info->creation_date; ?> <br />
+Money: <?php echo $info->cash; ?> <br />
 
 
 </body>
