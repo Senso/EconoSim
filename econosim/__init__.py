472201b24498923ff8726fe811b9038a2b35ec64
Sensoz <sensoz@gmail.com>
2012-05-08 12:07:51 +0700

    removing template renderer

--- econosim/__init__.py
+++ econosim/__init__.py
@@ -19,8 +19,7 @@ def main(global_config, **settings):
     config.add_route('home', '/')
     
     config.add_route('login', '/login')
-    config.add_view(login, route_name='login',
-                    renderer=render_template, permission=NO_PERMISSION_REQUIRED)
+    config.add_view(login, route_name='login', permission=NO_PERMISSION_REQUIRED)
     
     config.scan()
     return config.make_wsgi_app()
