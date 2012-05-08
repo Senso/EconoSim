f0026e154be456197741f2de57a57b11f1961f50
Sensoz <sensoz@gmail.com>
2012-05-08 11:51:28 +0700

    Testing import

--- econosim/__init__.py
+++ econosim/__init__.py
@@ -1,9 +1,8 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
-from apex.models import initialize_sql
-
 from .models import DBSession
+from .models import initialize_sql
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
