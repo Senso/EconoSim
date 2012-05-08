252120d7899a7d713e27760f6fc0a4a34050c3ea
Senso <sensoz@gmail.com>
2012-05-08 22:48:47 +0700

    Placeholder market controller

---
+++ application/controllers/market.php
@@ -0,0 +1,15 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Market extends CI_Controller {
+    
+	function __construct() {
+		parent::__construct();
+
+		$this->load->helper('url');
+		$this->load->library('tank_auth');
+		$this->load->model('Company_model');
+		
+        if (!$this->tank_auth->is_logged_in()) {
+			redirect('/auth/login/');
+		}
+	}
