e207aff75ebc1ecc54eab94232afac9124afa592
Sensoz <sensoz@gmail.com>
2012-05-08 12:01:27 +0700

    copying login from Apex

--- econosim/__init__.py
+++ econosim/__init__.py
@@ -4,6 +4,8 @@ from sqlalchemy import engine_from_config
 from .models import DBSession
 from .models import initialize_sql
 
+from views import login
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
@@ -15,6 +17,11 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    
+    config.add_route('login', '/login')
+    config.add_view(login, route_name='login',
+                    renderer=render_template, permission=NO_PERMISSION_REQUIRED)
+    
     config.scan()
     return config.make_wsgi_app()
 
--- econosim/views.py
+++ econosim/views.py
@@ -5,30 +5,44 @@ from sqlalchemy.exc import DBAPIError
 
 from .models import (
     DBSession,
-    MyModel,
     )
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name=='one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one':one, 'project':'EconoSim'}
+#@view_config(route_name='home', renderer='templates/mytemplate.pt')
+def login(request):
+    """ login(request)
+    No return value
 
-conn_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
+    Function called from route_url('apex_login', request)
+    """
+    title = _('You need to login')
+    came_from = get_came_from(request)
+    if not apex_settings('exclude_local'):
+        if asbool(apex_settings('use_recaptcha_on_login')):
+            if apex_settings('recaptcha_public_key') and apex_settings('recaptcha_private_key'):
+                LoginForm.captcha = RecaptchaField(
+                    public_key=apex_settings('recaptcha_public_key'),
+                    private_key=apex_settings('recaptcha_private_key'),
+                )
+            form = LoginForm(request.POST,
+                            captcha={'ip_address': request.environ['REMOTE_ADDR']})
+        else:
+            form = LoginForm(request.POST)
+    else:
+        form = None
 
-1.  You may need to run the "initialize_EconoSim_db" script
-    to initialize your database tables.  Check your virtual 
-    environment's "bin" directory for this script and try to run it.
+    velruse_forms = generate_velruse_forms(request, came_from)
 
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
+    if request.method == 'POST' and form.validate():
+        user = AuthUser.get_by_login(form.data.get('login'))
+        if user:
+            headers = apex_remember(request, user)
+            return HTTPFound(location=came_from, headers=headers)
 
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+    return {'title': title, 'form': form, 'velruse_forms': velruse_forms, \
+            'form_url': request.route_url('apex_login'),
+            'action': 'login'}
+    
+
+
+
 
