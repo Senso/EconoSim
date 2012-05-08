7d84d6a4416a6b40608b80fccdb073b2a2b01925
Senso <sensoz@gmail.com>
2012-05-08 21:00:36 +0700

    more detailed info

--- application/views/company_info.php
+++ application/views/company_info.php
@@ -4,10 +4,6 @@
 </head>
 <body>
 
-<?php
-    print_r($owner_info);
-?>
-
 Name: <?php echo $info->name; ?> <br />
 Owner: <a href='/user/info/<?php echo $info->owner;?>'><?php echo $owner_info->username; ?></a> <br />
 Created: <?php echo $info->creation_date; ?> <br />
