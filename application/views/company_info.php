b128b72f93e665e91789d880c2cf85a7b5af8b2b
Sensoz <sensoz@gmail.com>
2012-05-09 10:15:39 +0700

    added dollar sign

--- application/views/company_info.php
+++ application/views/company_info.php
@@ -2,5 +2,5 @@
 Name: <?php echo $info->name; ?> <br />
 Owner: <a href='/user/info/<?php echo $info->owner;?>'><?php echo $owner_info->username; ?></a> <br />
 Created: <?php echo $info->creation_date; ?> <br />
-Money: <?php echo $info->cash; ?> <br />
+Money: $<?php echo $info->cash; ?> <br />
 
