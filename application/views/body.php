a649d3cabe5d7dd39e5b6f45c853afd609f89c57
Sensoz <sensoz@gmail.com>
2012-05-09 09:52:36 +0700

    template mods

--- application/views/body.php
+++ application/views/body.php
@@ -1,7 +1,7 @@
-<?php
-    $this->load->view('header');
-    
-    echo $content;
-    
-    $this->load->view('footer');
-?> 
+<?php $this->load->view('header'); ?>
+
+<div id="main">
+    <?php echo $content; ?>
+</div>
+
+<?php $this->load->view('footer'); ?>
