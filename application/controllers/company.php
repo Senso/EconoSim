deaedd1abdb8ec3dbcb1562ab972687d15a9e0b2
Sensoz <sensoz@gmail.com>
2012-05-09 14:00:57 +0700

    fixed array

--- application/controllers/company.php
+++ application/controllers/company.php
@@ -54,7 +54,7 @@ class Company extends CI_Controller {
 			$user_id = $this->tank_auth->get_user_id();
 			$comp = $this->Company_m->get_company_by_user($user_id);
 			
-			$data = $this->Company_m->get_buildings_by_company($comp[0]->id);
+			$data = $this->Company_m->get_buildings_by_company($comp->id);
 			
 			$this->template->show('company_buildings', 'Buildings', $data);
 		}
