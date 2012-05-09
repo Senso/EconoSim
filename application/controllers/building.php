cb480efc7e349a636aaa7b4a36f7df734c2f3366
Sensoz <sensoz@gmail.com>
2012-05-09 17:27:18 +0700

    working on production

--- application/controllers/building.php
+++ application/controllers/building.php
@@ -6,7 +6,6 @@ class Building extends CI_Controller {
 		parent::__construct();
 
 		$this->load->helper('url');
-        $this->load->helper('form');
 		$this->load->library('tank_auth');
 		$this->load->model('Building_m');
 	}
@@ -16,11 +15,11 @@ class Building extends CI_Controller {
         
 		$data['info'] = $this->Building_m->get_building_by_id($b_id);
         
+        // This builds the array of stuff this specific building can produce.
         $data['select'] = array();
         $prod = $this->Building_m->get_possible_production($b_id);
         if ($prod) {
             foreach ($prod as $key => $product) {
-                //$prods_req = $product->products_required;
                 $prod_id     = $product->id;
                 $output_name = $product->name;
                 
@@ -31,7 +30,8 @@ class Building extends CI_Controller {
         $this->template->show('building_info', 'Building Info', $data);
 	}
     
-    function production($b_id) {
+    function production() {
+        $this->input->post('choose_prod')
         
     }
     
--- application/views/building_info.php
+++ application/views/building_info.php
@@ -1,13 +1,14 @@
 
 <br />
-<?php print_r($select); ?>
-<br />
 <strong>Building name:</strong> <?php echo $info->name ?> <br />
 <strong>Built on:</strong> <?php echo $info->created ?> <br />
 <br />
 <Strong>Set Production:</strong>
-<?php
-    echo form_open('building/production');
+<form action="/building/production" method="post" accept-charset="utf-8">
+    <?php echo form_dropdown('choose_prod', $select); ?>
+    <input type="submit" value="Submit" />
+    
+</form>
     
     
 ?>
